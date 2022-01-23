487afc6605bf841dc67d7443e08201c5d6031235
Anuj Yadav <anujpy.acct@gmail.com>
2022-01-23 12:05:56 +0530

    firebase config file ADDED🔧

---
+++ src/configuration/firebase/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvEakW_1IONaNUVAwfes5gjD5Js3bFLyM",
+  authDomain: "snapphotos-6ee9f.firebaseapp.com",
+  projectId: "snapphotos-6ee9f",
+  storageBucket: "snapphotos-6ee9f.appspot.com",
+  messagingSenderId: "882220130368",
+  appId: "1:882220130368:web:00f2b49cce20774232bfa5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
